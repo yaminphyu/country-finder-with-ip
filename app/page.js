cef5b8ae5b5c5d27335fea79a9b57a730fd3bd29
yamin <yamin148@infinity.gmail.com>
2024-06-08 15:02:38 +0700

    added isFetch

--- app/page.js
+++ app/page.js
@@ -1,52 +1,28 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [jsonData, setJsonData] = useState({
-    id: '',
-    city: '',
-    region: '',
-    country: '',
-    loc: '',
-    org: '',
-    timezone: '',
-  });
-  // const fetchCountry = async () => {
-  //   try {
-  //       const response = await fetch('/api/getCountry');
-  //       const data = await response.json();
-  //       setJsonData({
-  //         id: data.id,
-  //         city: data.city,
-  //         region: data.region,
-  //         country: data.country,
-  //         loc: data.loc,
-  //         org: data.org,
-  //         timezone: data.timezone
-  //       });
-  //   } catch (error) {
-  //       console.error('Error fetching country:', error);
-  //   }
-  // };
+  const [isFetch, setIsFetch] = useState(true);
+  const fetchCountry = async () => {
+    try {
+        const response = await fetch('/api/getCountry');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching country:', error);
+    }
+  };
 
-  // fetchCountry();
+  useEffect(() => {
+    if (isFetch) {
+      fetchCountry();
+      setIsFetch(false);
+    }
+  }, [isFetch]);
 
   return (
     <>
       <h1>Hello</h1>
-      {
-        jsonData && (
-          <div>
-            <p>{jsonData.id}</p>
-            <p>{jsonData.city}</p>
-            <p>{jsonData.region}</p>
-            <p>{jsonData.country}</p>
-            <p>{jsonData.loc}</p>
-            <p>{jsonData.org}</p>
-            <p>{jsonData.timezone}</p>
-          </div>
-        )
-      }
     </>
   );
 }
