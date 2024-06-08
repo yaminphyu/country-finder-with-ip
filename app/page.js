6a2e6a409c26ef2910400bcef77223f6a69f5b2d
yamin <yamin148@infinity.gmail.com>
2024-06-08 14:59:02 +0700

    show ui of

--- app/page.js
+++ app/page.js
@@ -11,25 +11,25 @@ export default function Home() {
     org: '',
     timezone: '',
   });
-  const fetchCountry = async () => {
-    try {
-        const response = await fetch('/api/getCountry');
-        const data = await response.json();
-        setJsonData({
-          id: data.id,
-          city: data.city,
-          region: data.region,
-          country: data.country,
-          loc: data.loc,
-          org: data.org,
-          timezone: data.timezone
-        });
-    } catch (error) {
-        console.error('Error fetching country:', error);
-    }
-  };
+  // const fetchCountry = async () => {
+  //   try {
+  //       const response = await fetch('/api/getCountry');
+  //       const data = await response.json();
+  //       setJsonData({
+  //         id: data.id,
+  //         city: data.city,
+  //         region: data.region,
+  //         country: data.country,
+  //         loc: data.loc,
+  //         org: data.org,
+  //         timezone: data.timezone
+  //       });
+  //   } catch (error) {
+  //       console.error('Error fetching country:', error);
+  //   }
+  // };
 
-  fetchCountry();
+  // fetchCountry();
 
   return (
     <>
